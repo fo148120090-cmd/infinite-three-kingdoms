6b8a1fc4add6b7536d9cfe3d90fe740c64aea536
fo148120090-cmd <fo148120090@gmail.com>
2026-09-15 03:23:13 +0900

    refactor: add board constants

--- src/game/data/constants.ts
+++ src/game/data/constants.ts
@@ -1,3 +1,5 @@
+export const BOARD_WIDTH = 7;
+export const BOARD_HEIGHT = 6;
 export const SAVE_KEY = 'infinite-three-kingdoms-save-v2';
 export const MAX_EQUIPMENT_LEVEL = 20;
 export const MAX_GENERAL_STARS = 6;
