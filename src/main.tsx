e50dcacded7fde1b94cba328e35d2a6e4763ef23
fo148120090-cmd <fo148120090@gmail.com>
2026-09-15 04:47:08 +0900

    Use React app shell for mobile UX

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import AppShell from './AppShell';
 import './styles.css';
 
-createRoot(document.getElementById('root')!).render(<React.StrictMode><App /></React.StrictMode>);
+createRoot(document.getElementById('root')!).render(<React.StrictMode><AppShell /></React.StrictMode>);
