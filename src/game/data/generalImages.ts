4c57f9d3447e6dfac4665ddf3ffa2bec581fb9b6
fo148120090-cmd <fo148120090@gmail.com>
2026-09-18 23:37:51 +0900

    chore: remove legacy Three Kingdoms assets

--- src/game/data/generalImages.ts
+++
@@ -1,9 +0,0 @@
-import type { General } from '../types';
-
-const BASE='/infinite-three-kingdoms/images/generals';
-export function getGeneralImagePaths(g:Pick<General,'id'>){
-  return {
-    sd: `${BASE}/${g.id}-sd.svg`,
-    portrait: `${BASE}/${g.id}-portrait.svg`,
-  };
-}
