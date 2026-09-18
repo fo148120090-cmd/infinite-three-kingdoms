cc838312d40a8e480db54e1de6e692b9c2519b33
fo148120090-cmd <fo148120090@gmail.com>
2026-09-18 23:39:35 +0900

    fix: make autonomous dungeon App the only runtime entry

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import AppShell from './AppShell';
-import './styles.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./styles.css";
 
-createRoot(document.getElementById('root')!).render(<React.StrictMode><AppShell /></React.StrictMode>);
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
