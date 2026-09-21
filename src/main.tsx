21ff7d0ab243876f91dc8caf4121c4b966516d49
fo148120090-cmd <fo148120090@gmail.com>
2026-09-21 10:22:42 +0900

    ui: remove intrusive Arden floating popup

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import BlacksmithNpc from "./BlacksmithNpc";
 import "./styles.css";
 import "./blacksmithNpc.css";
 
@@ -9,7 +8,6 @@ createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <>
       <App />
-      <BlacksmithNpc />
     </>
   </React.StrictMode>
 );
